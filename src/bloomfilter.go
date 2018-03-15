9513e2a511382f905c3605e48dcc0cbb59dabf02
贾伟川 <wellschuan@gmail.com>
2018-03-16 04:41:42 +0800

    Add BloomFilter constructor method

--- src/bloomfilter.go
+++ src/bloomfilter.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	_ "hash"
 	_ "hash/fnv"
+	"math"
 )
 
 type Interface interface {
@@ -12,41 +13,51 @@ type Interface interface {
 }
 
 type BloomFilter struct {
-	arrayBuffer []byte
-	a           uint
-	m           uint
+	arrayBuffer []bool
+	m 			uint
 	k           uint
 	n           uint
-	v 			uint
+	v           uint
 }
 
-func (bf *BloomFilter) New(size) *BloomFilter {
-	return &BloomFilter{
-		arrayBuffer: make([]bool, size),
-		a:           0,
+
+
+func New(size uint, k uint) BloomFilter {
+
+	//var n = math.Ceil(float64(size) / 32)
+	var kbytes = math.Ceil(math.Log(math.Ceil(math.Log(float64(size)) / math.Ln2 / 8)) / math.Ln2)
+	kbytes = 1 << uint(kbytes)
+	var arrayBuffer = make([]bool, uint(kbytes) * k)
+	//var bucket = [uint(n)]int{}
+	return BloomFilter{
+		arrayBuffer: arrayBuffer,
 		m:           size,
-		k:           3, // we have 3 hash functions for now
+		k:           k, // we have 3 hash functions for now
 		n:           uint(0),
-		v:           0,
 	}
 }
 
-func (bf *BloomFilter) locations(v) float64 {
+func (bf BloomFilter) locations(v string) float64 {
+	
+	//var k = bf.k
+	//var m = bf.m
+	//var a = fnv_1a(v)
+	//var b = fnv_1a(v, 1576284489)
+	//
+	//return
+}
+
+func (bf BloomFilter) add(v) float64 {
 	fmt.Printf(v)
 	return v
 }
 
-func (bf *BloomFilter) add(v) float64 {
+func (bf BloomFilter) test(v) float64 {
 	fmt.Printf(v)
 	return v
 }
 
-func (bf *BloomFilter) test(v) float64 {
-	fmt.Printf(v)
-	return v
-}
-
-func (bf *BloomFilter) size(v) float64  {
+func (bf BloomFilter) size(v) float64  {
     fmt.Printf(v)
 	return v
 }
